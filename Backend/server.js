cb30042965b7a6f6926f3ee828c941ca1d7c5ead
Ijlal Furqaan <45873251+IjlalFurqaan@users.noreply.github.com>
2024-10-02 15:22:34 +0200

    login route

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from './routes/auth.routes.js'
 const app = express();
 
 dotenv.config();
@@ -9,6 +10,6 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`This is running on Port ${PORT}`));
