93a6fc05ec5af8b1539cd2d0833596c342518586
Ijlal Furqaan <45873251+IjlalFurqaan@users.noreply.github.com>
2024-10-03 19:45:01 +0200

    mongo

--- Backend/server.js
+++ Backend/server.js
@@ -8,10 +8,14 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
+
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.listen(PORT, () => {
 	connectToMongoDB();
