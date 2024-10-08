4e0d19181943977f7d4e5b1a339237c4ba52d77f
Ijlal Furqaan <45873251+IjlalFurqaan@users.noreply.github.com>
2024-10-08 23:32:27 +0200

    .

--- Backend/server.js
+++ Backend/server.js
@@ -13,9 +13,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+
 
 app.listen(PORT, () => {
 	connectToMongoDB();
